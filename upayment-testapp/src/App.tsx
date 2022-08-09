518e4acf8d387fd1b8f0accc55e4837730d6fb0e
cleancoder22 <cleancoder22@gmail.com>
2022-08-09 08:18:47 +0530

    feat(homePage) : new product component to display the list of products

--- upayment-testapp/src/App.tsx
+++ upayment-testapp/src/App.tsx
@@ -1,11 +1,15 @@
-import './App.css';
+import "./App.css";
+import HomePage from "./pages/productPage/HomePage";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Simple React Typescript Tailwind Sample
-    </h1>
-  );  
+    <>
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Simple React Typescript Tailwind Sample
+      </h1>
+      <HomePage />
+    </>
+  );
 }
 
 export default App;
